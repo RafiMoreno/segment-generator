6d98fb41b690dba64c83d16f87d989fefd4da51f
RafiMoreno <ahmadarrafi.m@gmail.com>
2026-09-16 16:34:44 +0700

    feat: define canvas schema and create initial seed data

--- segmentAPI/models.py
+++ segmentAPI/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Canvas(models.Model):
+    node = models.TextField(max_length=100)
+    port = models.TextField(max_length=100)
+    value = models.CharField(max_length=100, null=True)
+    source = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, related_name='source_port')
+    target = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, related_name='target_port')
+
